168af9925052985aed542254d3011dece04c5c56
Anrew <andrew.murray@osovega.com>
2020-02-18 16:37:39 -0600

    making some asyncin and bloggin

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,30 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { Post } from '@/interfaces/Post'
+
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    posts: [] as Post[]
+  },
+  mutations: {
+    getPosts(state, posts: Post[]) {
+      state.posts = posts;
+      console.log(state.posts)
+    }
+  },
+  actions: {
+    async getPosts({ commit }) {
+      try {
+        const result = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const posts = await result.json();
+        commit('getPosts', posts)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+  },
   modules: {}
 });
---
+++ src/views/Posts.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>PaRTY</h1>
+    <ul>
+      <li v-for="post of posts" :key="post.id">{{ post.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script lang='ts'>
+import { Component, Vue } from "vue-property-decorator";
+import store from "@/store";
+import { Post } from "@/interfaces/Post";
+
+@Component({
+  components: {}
+})
+export default class Posts extends Vue {
+  private posts: Post[];
+  constructor() {
+    super();
+    this.posts = [];
+  }
+
+  async created() {
+    try {
+      this.posts = await this.$store.dispatch("getPosts");
+      console.log(this.posts)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+</script>
+
+<style>
+</style>
+
+
+
+
